edbb24b4f968361cfa3e3a65e966f0f39d2b573a
SecretaryKevin <dtempler@outlook.co.nz>
2021-03-26 09:51:04 +1300

    Started the beginning of the dictionary generators

--- Improved_data_reformating.py
+++ Improved_data_reformating.py
@@ -1,4 +1,5 @@
 import http.client
+import re
 
 conn = http.client.HTTPSConnection("zootmc.com")
 
@@ -12,55 +13,50 @@ data = res.read()
 int_data = data.decode("utf-8")
 
 
-def initial_processing(int_string, processed_list=[]):
-    temp_list = list(int_string.split('","'))
-    temp_list = temp_list[1:-1]
-    for string in temp_list:
-        string.replace("|", "")
-        string.replace(",", "")
-        processed_list.append(string)
-    return processed_list
+def uuid_handler(int_uuid):
+    int_uuid = int_uuid.split(":")
+    int_uuid = int_uuid[-1]
+    return int_uuid.replace('"', "")
 
 
-def uuid_handler(string_uuid):
-    """removes the junk around the uuid and returns the cleaned uuid """
-    uuid = string_uuid.split(":")[-1].replace('"', "")
-    return uuid
+def key_stealer(list, temp_keys=["uuid"]):
+    uuid = list[0]
+    list.pop(0)
+    uuid = uuid_handler(uuid)
+    for items in list:
+        temp = items.find(":")
+        temp_keys.append(items[:temp - 1])
+    return temp_keys, uuid
 
 
-def space_remover(spaced_list):
-    """removes the extra spaces in the list passes back"""
-    while '' in spaced_list:
-        spaced_list.remove("")
-    return spaced_list
+def internal_dictionary_generator(input_list, directory={}, temp_list={}):
+    input_list.remove('"{0}":"').format(keys)
 
 
-def dictionary_generator(input_list, dictionary={}):
-    """creates the inner dictionary using the supplied data from the handler"""
-    for item in input_list:
-        if ':' in item:
-            temp = item.split(":")
-            if temp[1].isdigit() or "." in temp[1]:
-                dictionary[temp[0]] = float(temp[1])
-            else:
-                dictionary[temp[0]] = str(temp[1])
-        else:
-            dictionary[temp[0]].append(item)
-    return dictionary
 
 
-def dictionary_generator_handler(processed_list, final_dictionary={}, x=0):
-    keys = ["uuid", "kills", "death", "damage", "accuracy", "world", "movement", "inventory", "interactions"]
-    """keys = key_generator(processed_list)"""
-    while x < len(keys):
-        if x == 0:
-            final_dictionary[keys[0]] = uuid_handler(processed_list[0])
-        else:
-            final_dictionary[keys[x]] = space_remover(processed_list[x].split(" ")[1:])
-        x + 1
-    return final_dictionary
+
+    return directory
 
 
-test = initial_processing(int_data)
+def outter_dictionary_generator(rtp_list, keys, uuid, directory={}, x=0):
+    if len(directory) == 0:
+        directory[keys[0]] = uuid
+    else:
+        for data in rtp_list:
+            x = x + 1
+            directory[keys[x]] = internal_dictionary_generator(data)
+    return directory
 
-print(dictionary_generator_handler(test))
+
+int_data = int_data.replace("|", " ")
+int_data = int_data.replace(",", "")
+int_list = int_data.split('""')
+int_list = int_list[1:]
+print(int_list)
+global keys
+keys, uuid = key_stealer(int_list)
+final_dictionary = outter_dictionary_generator(int_list[], keys, uuid)
+print(final_dictionary)
+print(keys)
+print(uuid)
